45f141c5dfaecd9dca9eb979b68ace965f470f3b
lcd299 <lin-299@163.com>
2017-07-28 10:19:29 +0800

    公共头样式

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,11 +1,25 @@
 <template>
+  <div>
+    <div class="page-cover"></div>
+    <header class="header">
+      <header class="bar bar-nav">
+        <div class="pull-left">
+          <span class="iconfont icon-fenlei"></span>
+        </div>
+        <div class="title">{{headerTitle}}</div>
+        <div class="pull-right">
+          <span class="iconfont icon-sousuo_sousuo"></span>
+        </div>
+      </header>
+    </header>
 
+  </div>
 </template>
 
 <script>
     export default {}
 </script>
 
-<style scoped>
-
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  @import "header.styl";
 </style>
